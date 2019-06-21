25dd5cfa803402df9df597f655fc9cbe41b5704a
drinkeewu <773119690@qq.com>
2019-06-21 23:43:20 +0800

    部署文件更新

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'development' ? '' : '/auto-calculator/dist/'
+  publicPath: process.env.NODE_ENV === 'development' ? '' : '/bill-calculator/dist/'
 }
